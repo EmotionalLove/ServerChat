8b7f762e3c648970baa278b96175fe94466e6b29
EmotionalLove <sashwho.w@gmail.com>
2023-09-04 05:54:48 -0400

    - Move kickmsg logic to prevent concurrentmodification

--- src/main/java/online/calamitycraft/serverchat/mixin/ServerConfigurationManagerMixin.java
+++ src/main/java/online/calamitycraft/serverchat/mixin/ServerConfigurationManagerMixin.java
@@ -1,5 +1,6 @@
 package online.calamitycraft.serverchat.mixin;
 
+import net.minecraft.core.net.command.TextFormatting;
 import net.minecraft.core.net.packet.Packet3Chat;
 import net.minecraft.core.util.helper.AES;
 import net.minecraft.server.entity.player.EntityPlayerMP;
@@ -9,6 +10,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 
@@ -30,6 +35,11 @@ public class ServerConfigurationManagerMixin {
             ss.forEach(split -> sendEncrypted(this.playerEntities, split.trim()));
         } else sendEncrypted(playerEntities, message);
 
+    }
+
+    @Inject(method = "savePlayerStates", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/world/save/IPlayerFileData;writePlayerData(Lnet/minecraft/core/entity/player/EntityPlayer;)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT)
+    public void savePlayerStates(CallbackInfo ci, int i) {
+        this.playerEntities.get(i).playerNetServerHandler.kickPlayer(TextFormatting.ORANGE + "You have been disconnected from the server");
     }
 
 }
